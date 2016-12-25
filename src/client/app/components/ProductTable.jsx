18335a4c761f44a924e29e44602b2aa1a3b9f9b1
Jorge Reyero <jorgereyero@gmail.com>
2016-12-25 15:10:36 +0000

    create filterable table

---
+++ src/client/app/components/ProductTable.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import ProductRow from './ProductRow.jsx';
+import ProductCategoryRow from './ProductCategoryRow.jsx';
+
+class ProductTable extends React.Component {
+  render() {
+    var rows = [];
+    var lastCategory = null;
+    this.props.products.forEach((product) => {
+      if (product.name.indexOf(this.props.filterText) === -1 || (!product.stocked && this.props.inStockOnly)) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+      }
+      rows.push(<ProductRow product={product} key={product.name} />);
+      lastCategory = product.category;
+    });
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+export default ProductTable;
---
+++ src/client/app/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+import FilterableProductTable from './components/FilterableProductTable.jsx';
+
+
+class App extends React.Component {
+  render () {
+  var PRODUCTS = [
+    {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+    {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+    {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+    {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+    {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+    {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+  ];
+    return (
+        <FilterableProductTable products={PRODUCTS} />
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
